6055c1f5eeb5378cd30ef12229a24459546b442e
martin <martin.cadez0@gmail.com>
2025-12-03 17:55:37 +0100

    feat: implement cubic-graph generator logic

--- src/objects.py
+++ src/objects.py
@@ -1,8 +1,7 @@
+from sage.all import *
 import numpy as np
 import networkx as nx
 from networkx.algorithms import bipartite
-
-import networkx as nx
 import matplotlib.pyplot as plt
 
 
@@ -139,14 +138,97 @@ class BipartiteGraph:
         else:
             plt.show()
 
+class CubicGraphs:
+    NODE_COLOR = "#698ad1"
+
+    def __init__(self, num_nodes: int):
+        self.num_nodes = num_nodes
+        self.label = f"CubicGraphs({num_nodes})"
+        self.graphs = self._build_graphs()
+    
+    def __repr__(self) -> str:
+        return f"CubicGraphs(num_nodes={self.num_nodes}, num_graphs={len(self.graphs)})"
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+    @staticmethod
+    def _validate_args(num_nodes: int) -> None:
+        if not isinstance(num_nodes, int):
+            raise ValueError("num_nodes must be an integer.")
+        if num_nodes < 4:
+            raise ValueError("Cubic graph must have at least 4 nodes.")
+        if num_nodes % 2 != 0:
+            raise ValueError("Cubic (3-regular) graph must have an even number of nodes.")
+
+    def _build_graphs(self):
+        self._validate_args(self.num_nodes)
+        
+        nx_graphs = [graph.networkx_graph() for graph in graphs(self.num_nodes)
+                     if graph.is_regular(3) and graph.is_connected()]
+
+        if not nx_graphs:
+            raise RuntimeError
+
+        return nx_graphs
+
+    @property
+    def edges(self) -> list[list[tuple[int, int]]]:
+        return [list(graph.edges()) for graph in self.graphs]
+
+    @property
+    def adj_matrices(self) -> list[np.ndarray]:
+        return [nx.to_numpy_array(graph, nodelist=sorted(graph.nodes())) for graph in self.graphs]
+
+    @property
+    def incidence_matrices(self) -> list[np.ndarray]:
+        return [nx.incidence_matrix(graph, oriented=False).toarray() for graph in self.graphs]
+
+    def plot(self, index: int | None = None, filename: str | None = None) -> None:
+        if index is not None:
+            self._plot_single(self.graphs[index], index, filename)
+        else:
+            for i, graph in enumerate(self.graphs):
+                self._plot_single(graph, i)
+
+    def _plot_single(self, graph: nx.Graph, index: int, filename: str | None = None):
+        plt.figure(figsize=(10, 8))
+        pos = nx.circular_layout(graph)
+        
+        nx.draw_networkx(
+            graph,
+            pos=pos,
+            labels={n: f"$v_{{{n+1}}}$" for n in graph.nodes()},
+            node_color=self.NODE_COLOR,
+            edgecolors="#292a40",
+            linewidths=2,
+            node_size=3000,
+            width=2,
+            font_color="white",
+            font_size=30
+        )
+        plt.axis("off")
+        plt.title(f"{self.label} - Graph {index+1}")
+        if filename:
+            plt.savefig(filename, bbox_inches="tight", facecolor="white")
+            plt.close()
+        else:
+            plt.show()
+
 
 if __name__ == "__main__":
-    G = BipartiteGraph(num_u=3, num_v=2, num_edges=4)
-    print(G)
-    print(G.u_nodes)
-    print(G.v_nodes)
-    print(G.edges)
-    print(G.degree_sequence)
-    print(G.adj_matrix)
-    print(G.incidence_matrix)
-    G.plot()
+    # G = BipartiteGraph(num_u=3, num_v=2, num_edges=4)
+    # print(G)
+    # print(G.u_nodes)
+    # print(G.v_nodes)
+    # print(G.edges)
+    # print(G.degree_sequence)
+    # print(G.adj_matrix)
+    # print(G.incidence_matrix)
+    # G.plot()
+
+    cg6 = CubicGraphs(4)
+    print("Number of graphs:", len(cg6.graphs))
+    cg6.plot(index=0, filename="test.png")
+    cg6.plot(index=1, filename="test1.png")
+
