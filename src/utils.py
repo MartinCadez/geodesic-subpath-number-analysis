1235fcbd3d3fcd692bf9708ce2569a5d7de524a0
martin <martin.cadez0@gmail.com>
2025-12-09 17:30:06 +0100

    feat(gpn): add option to exclude trivial paths

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,9 @@
 import networkx as nx
 
-def gpn(G: nx.Graph) -> int:
-    n = G.number_of_nodes()
-    total_paths = n 
-
+def gpn(G: nx.Graph, count_trivial:bool=TRUE) -> int:
+    total_paths = G.number_of_nodes() if count_trivial else 0
     nodes = list(G.nodes())
+
     for i, u in enumerate(nodes):
         for v in nodes[i+1:]:
             total_paths += sum(1 for _ in nx.all_shortest_paths(G, source=u, target=v))
