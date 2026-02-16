38702ede435ae3927ee871ee04cf23c620c7514a
martin <martin.cadez0@gmail.com>
2026-02-16 16:40:57 +0100

    refactor(utils): gpn fun optimise impl

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,40 @@
 import networkx as nx
+from collections import deque
 
 
-def gpn(G: nx.Graph, count_trivial: bool = True) -> int:
-    total_paths = G.number_of_nodes() if count_trivial else 0
+def gpn(
+    G: nx.Graph,
+    count_trivial: bool = True
+    ) -> int:
+    
+    gpn = G.number_of_nodes() if count_trivial else 0
     nodes = list(G.nodes())
 
-    for i, u in enumerate(nodes):
-        for v in nodes[i + 1 :]:
-            total_paths += sum(1 for _ in nx.all_shortest_paths(G, source=u, target=v))
+    # source to BFS
+    for source_index, source_node in enumerate(nodes):
 
-    return total_paths
+        dist = {source_node: 0}
+        sigma = {source_node: 1} # number of shortest paths to each node
+        queue = deque([source_node]) # bfs init
+
+        # BFS traversal
+        while queue:
+            current_node = queue.popleft()
+
+            for neighbor in G[current_node]:
+                
+                # if: no visit to neighbor
+                if neighbor not in dist:
+                    dist[neighbor] = dist[current_node] + 1
+                    sigma[neighbor] = sigma[current_node]
+                    queue.append(neighbor)
+
+                # if: neighbor already discovered at shortest distance
+                elif dist[neighbor] == dist[current_node] + 1:
+                    sigma[neighbor] += sigma[current_node]
+
+        # count number of shortest paht of source node
+        for target_node in nodes[source_index + 1:]:
+            gpn += sigma.get(target_node, 0)
+
+    return gpn
